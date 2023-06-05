186488e42c7f2f0b1d10a0554e2f21ce3850dd23
tahereh <tahereh.moeini@gmail.com>
2023-06-05 23:42:56 +0430

    add cat

--- post/views.py
+++ post/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpRequest,HttpResponse
-from .models import Post
+from .models import Post,Category
 
 # Create your views here.
 def index(request):
     posts=Post.objects.all()
+    categories=Category.objects.all()
     context={
-        'posts':posts
+        'posts':posts,
+        'categories': categories
     }
     return render(request,"post/index.html",context)
 
 def detail(request):
     return render(request,"post/detail.html")
 def category(request):
+    categories=Category.objects.all()
+    context={
+        'categories': categories
+    }
     return render(request,"post/category.html")
